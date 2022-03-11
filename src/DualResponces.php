fd07b55345c17c115b2485e0f0f746898bf31e8b
Mohammad Maleki <malekii24@outlook.com>
2022-03-11 07:39:24 +0330

    wrote response function

--- src/DualResponces.php
+++ src/DualResponces.php
@@ -2,7 +2,24 @@
 
 namespace Mlk9\DualResponces;
 
+use Illuminate\Support\Facades\Request;
+
 class DualResponces
 {
-    
+    /**
+     * with this function you can send dual responces 
+     *
+     * @param mixed $webResponse
+     * @param mixed $apiResponse
+     * @return mixed
+     */
+    public function response($webResponse, $apiResponse = null) : mixed
+    {
+        if(Request::is('api/*') && !is_null($apiResponse))
+        {
+            return $apiResponse;
+        }else{
+            return $webResponse;
+        }
+    }
 }
