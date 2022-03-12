f69bea06abf28c6d890348c61d9bba5edf81615f
Mohammad Maleki <malekii24@outlook.com>
2022-03-12 17:26:39 +0330

    refactor codes

--- src/DualResponses.php
+++ src/DualResponses.php
@@ -13,7 +13,7 @@ class DualResponses
     public function __construct()
     {
         $this->defualtResponse = [
-            'status' => true,
+            'status_result' => true,
             'status_code' => 200,
             'message' => _('request_successful'),
             'errors' => null,
@@ -33,13 +33,13 @@ class DualResponses
     {
         if(array_key_exists('errors',$apiResponse) && empty($apiResponse['errors']))
         {
-            $this->defualtResponse['status'] = false;
+            $this->defualtResponse['status_result'] = false;
             $this->defualtResponse['status_code'] = !array_key_exists('status_code',$apiResponse) ? 400 : $apiResponse['status_code'];
             $this->defualtResponse['message'] = !array_key_exists('message',$apiResponse) ? _('request_failed') : $apiResponse['message'];
         }
         if(array_key_exists('data',$apiResponse) && empty($apiResponse['data']))
         {
-            $this->defualtResponse['status'] = false;
+            $this->defualtResponse['status_result'] = false;
             $this->defualtResponse['status_code'] = !array_key_exists('status_code',$apiResponse) ? 404 : $apiResponse['status_code'];
             $this->defualtResponse['message'] = !array_key_exists('message',$apiResponse) ? _('not_found') : $apiResponse['message'];
             if($this->isWebRoute())
@@ -62,7 +62,7 @@ class DualResponses
 
         if($this->isApiRoute() && !is_null($apiResponse))
         {
-            return Response::json($mergedResponse,$this->defualtResponse['code']);
+            return Response::json($mergedResponse,$this->defualtResponse['status_code']);
         }else{
             return $webResponse;
         }
